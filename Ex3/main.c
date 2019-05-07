e7330c2964d641d9092bd6f86198450e6a62deba
netanelf <netanel.fayoumi@mail.huji.ac.il>
2019-05-07 19:56:05 +0300

    deleted old code from main

--- Ex3/main.c
+++ Ex3/main.c
@@ -9,13 +9,6 @@
 
 #define MODEM_PORT "COM5"
 
-#define PRINT_FORMAT "Iteration #%d \nLatitude:\n%d\nLongitude:\n%d\nAltitude:\n%d\nTime:\n%s\nGoogle Maps:\n%f, %f\n"
-//int main() {
-//    unsigned char buf[] = "\r\n+SYSSTART\r\n\r\nhello\r\n";
-//    unsigned char * tokensarray[10];
-//    splitBufferToResponses(buf, tokensarray);
-//    return 1;
-//}
 int main() {
     // Initialize the cellular modems.
     CellularInit(MODEM_PORT);
@@ -54,96 +47,4 @@ int main() {
     CellularDisable();
     exit(0);
 }
-//int old_main() {
-//    SerialInit("COM5", 115200);
-//
-//    unsigned char systart[] = "\r\n^SYSSTART\r\n";
-//    unsigned char buff[50] = "";
-//    int maxlen = 50;
-//    unsigned int timout_ms = 100;
-//
-//    do {
-//        SerialRecv(buff, maxlen, timout_ms);
-//        printf("loop - %s", buff);
-//    } while (memcmp(buff, systart, (size_t) 13) != 0);
-//
-//    fprintf(stderr, "end of loop\n");
-//
-//    unsigned char stopecho[] = "ATE0\r\n";
-//    if (!SerialSend(stopecho, sizeof("ATE0\r\n"))) {
-//        fprintf(stderr, "send error\n");
-//    }
-//    printf("buffer001: %s", buff);
-//    memset(buff, 0, 50);
-//
-//    SerialRecv(buff, maxlen, timout_ms);
-//    printf("buffer002: %s", buff);
-//    memset(buff, 0, 50);
-//
-//    unsigned char hello[] = "AT\r\n";
-//    if (!SerialSend(hello, sizeof("AT\r\n"))) {
-//        fprintf(stderr, "send error\n");
-//    }
-//    printf("buffer1: %s", buff);
-//    memset(buff, 0, 50);
-//    SerialRecv(buff, maxlen, timout_ms);
-//    printf("buffer2: %s", buff);
-//    memset(buff, 0, 50);
-//    printf("%s", buff);
-//
-//    unsigned char goodbye[] = "AT^SMSO\r\n";
-//    if (!SerialSend(goodbye, sizeof("AT^SMSO\r\n"))) {
-//        fprintf(stderr, "send error\n");
-//    }
-//    SerialDisable();
-//    exit(0);
-//
-////    GPSInit();
-////    GPS_INITIALIZED = true;
-////    uint32_t iteration_counter = 0;
-////    GPS_LOCATION_INFO* last_location = malloc(sizeof(GPS_LOCATION_INFO));
-////    if (last_location == NULL)
-////    {
-////        printf("Memory Allocation Error");
-////        return EXIT_FAILURE;
-////    }
-////
-////    bool result;
-////
-////    time_t init_time = time(NULL);
-////    time_t current_time;
-////    double diff_seconds;
-////
-////    float lat_deg;
-////    float long_deg;
-////    while (GPS_INITIALIZED) {
-////        iteration_counter++;
-////        result = GPSGetFixInformation(last_location);
-////        current_time = time(NULL);
-////        diff_seconds = difftime(current_time, init_time);
-////        // Google Maps format
-////        // Decimal degrees (DD): 41.40338, 2.17403
-////        if (result && diff_seconds >= 3){
-////
-////            lat_deg = (last_location->latitude) / 10000000.0;
-////            long_deg = (last_location->longitude) / 10000000.0;
-////            printf(PRINT_FORMAT,
-////                    iteration_counter,
-////                    last_location->latitude,
-////                    last_location->longitude,
-////                    last_location->altitude,
-////                    last_location->fixtime,
-////                    lat_deg,
-////                    long_deg);
-////
-////            fflush(stdout);
-////
-////            init_time = current_time;
-////        }
-////    }
-////    free(last_location);
-////    GPSDisable();
-//
-//    exit(0);
-//}
 

7e6048b8364eb9a13145d9de55bdd96c2df39224
netanelf <netanel.fayoumi@mail.huji.ac.il>
2019-05-19 01:30:10 +0300

    removed Delay

--- Ex3/main.c
+++ Ex3/main.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
 
-#include "serial_io.h"  // TODO REMOVE
+//#include "serial_io.h"  // TODO REMOVE
 #include "cellular.h"
 #include "string.h"
 
 #define MODEM_PORT "COM5"
-#define MAX_IL_CELL_OPS 10  //TODO check 10 is enough
+#define MAX_IL_CELL_OPS 20
 
 //TODO print program's progress
 int main() {
@@ -18,7 +19,8 @@ int main() {
     // Makes sure it’s responding to AT commands.
     // looping to check modem responsiveness.
     while (!CellularCheckModem()) {
-        Delay(10);
+        Sleep(10);
+//        Delay(10);
     }
 
     // Setting modem to deregister and remain unregistered.
@@ -26,21 +28,22 @@ int main() {
 
     // Finds all available cellular operators.
     int num_operators_found = 0;
-    printf("~~~ #ops found: %d ~~~", num_operators_found);
     OPERATOR_INFO operators_info[MAX_IL_CELL_OPS];
     printf("Finding all available cellular operators...");
     while (!CellularGetOperators(operators_info, MAX_IL_CELL_OPS, &num_operators_found)) {
         printf("...");
     }
     printf("\n");
+    printf("~~~ #ops found: %d ~~~", num_operators_found); //TODO delete
 
     // Tries to register with each one of them (one at a time).
     printf("Trying to register with each one of them (one at a time)\n");
     for (int op_index = 0; op_index <= num_operators_found; op_index++) {
-//        // Deregister from current operator TODO check is this needed?
-//        while (!CellularSetOperator(DEREGISTER, NULL));
+        // Deregister from current operator TODO check is this needed?
+        while (!CellularSetOperator(DEREGISTER, NULL));
 
         // register to operator
+        printf("Trying to register with %s...\n", operators_info[op_index].operatorName);
         if (CellularSetOperator(SPECIFIC_OP, operators_info[op_index].operatorName)) {
 
             // If registered successfully, it prints the signal quality.
@@ -56,13 +59,19 @@ int main() {
                 int signal_quality = -1;
                 if (CellularGetSignalQuality(&signal_quality)) {
                     printf("Current signal quality: %d\n", signal_quality);
-                    continue;
+                } else if (signal_quality == -1){
+                    printf("Modem didn't respond\n");
+                } else if (signal_quality == 99) {
+                    printf("\"Current signal quality is UNKNOWN\n");
                 }
+                continue;
             }
         }
         printf("Modem registration failed.\n");
     }
+
     // Print the program’s progress all along (e.g. “Checking modem…the modem is ready!”  etc.)
+    printf("Disabling Cellular and exiting..,\n");
     CellularDisable();
     exit(0);
 }
